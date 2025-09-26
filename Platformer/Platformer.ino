8357f4e55f7440aab7b0bc2d92f5066530d5690f
Michael Fewkes <mdfewkes@gmail.com>
2025-09-25 19:31:43 -0700

    Test loading map

--- Platformer/Platformer.ino
+++ Platformer/Platformer.ino
@@ -28,6 +28,10 @@ void loop() {
 
 			if (arduboy.justPressed(A_BUTTON)) {
 				gameState = GameState::Gameplay;
+
+				LoadRoom(level[0], 0);
+				LoadRoom(level[1], 1);
+				LoadRoom(level[2], 2);
 			}
 			break;
 		
